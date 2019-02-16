f694b9f544fe825f58ff932428fce6f619018356
Dandysey <dandysey@gmail.com>
2019-02-16 13:20:44 +0000

    Initial commit

---
+++ learning-kotlin/src/Practice.kt
@@ -0,0 +1,13 @@
+
+
+
+
+class Pupil  {
+        var FirstName: String= "Hello World"
+        var LastName:String= ""
+        var Address:String= ""
+        var Height: Double= 0.0
+
+        println("")
+
+}
---
+++ learning-kotlin/src/tutorial/Tutorial.kt
@@ -0,0 +1,25 @@
+package tutorial
+
+ /*
+  This is a variable which has
+  an explicitly declared data type
+
+  */
+var name: String = " dandysey "
+/* This is a variable which has
+  an implicitly declared data type
+
+
+ */
+var username = " js afatsiawo "
+
+
+fun main() {
+    // This is a single line comment
+    println(name)
+    println(username)
+    //we updated the value of the name field
+    name= "Daniel Owusu"
+    println( name)
+    // a variable is sa
+}
